d1ea4003559a8a58a6c44fed51bd9894490e7ead
syntaxaire <syntaxaire@gmail.com>
2022-04-07 19:48:15 -0400

    Add fake customer generator

---
+++ fakers.py
@@ -0,0 +1,14 @@
+"""Fake generators for ZohoCRMFaker"""
+from faker import Faker
+
+
+fake = Faker()
+
+
+class FakeCustomer:
+    """Class to represent a fake customer"""
+    def __init__(self):
+        """Initialize a fake customer"""
+        self.name: str = fake.name()
+        self.address: str = fake.address()
+        self.phone: str = fake.phone_number()
