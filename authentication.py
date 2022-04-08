c37768b52365645e44f220899023aece1c04ccd7
syntaxaire <syntaxaire@gmail.com>
2022-04-07 21:20:16 -0400

    Working implementation to insert one fake contact

--- authentication.py
+++ authentication.py
@@ -1,7 +1,25 @@
 """Module for OAuth 2.0 authentication to Zoho CRM"""
-from oauthlib.oauth2 import BackendApplicationClient
+import requests
 
 from config import config
 
 
-client = BackendApplicationClient(config['Zoho CRM token'])
+ACCOUNTS_URL = 'https://accounts.zoho.com'  # US site
+
+
+def get_access_token(refresh_token: str) -> str:
+    """Use our stored refresh token to generate an access token.
+    This lasts an hour according to:
+    https://www.zoho.com/crm/developer/docs/api/v2/refresh.html
+    """
+    url = f'{ACCOUNTS_URL}/oauth/v2/token'
+    params = {'refresh_token': refresh_token,
+              'client_id': config['Client ID'],
+              'client_secret': config['Client Secret'],
+              'grant_type': 'refresh_token',
+              }
+    response = requests.post(url, params=params)
+    reply = response.json()
+    if 'access_token' not in reply:
+        raise Exception(reply)
+    return reply['access_token']
--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 """Entry point to ZohoCRMFaker"""
-import yaml
+import requests
 
+from authentication import get_access_token
 from config import config
-from authentication import client
+from fakers import FakeCustomer
+
+
+URL = 'https://www.zohoapis.com/crm/v2/Contacts/upsert'
 
 
 def main():
-    """Set up environment for loading fake data."""
+    """Load fake data into the CRM."""
+    access_token = get_access_token(config['Refresh token'])
+    fake = FakeCustomer()
+    headers = {'Authorization': f'Zoho-oauthtoken {access_token}'}
+    data = {
+        'data': [
+            {'Last_Name': fake.last_name,
+             'Email': fake.email,
+             'Company': 'None',
+             'Phone': fake.phone,
+             'Address': fake.address,
+             'Lead_Status': 'Contacted',
+             }
+        ]
+    }
+    response = requests.post(url=URL, headers=headers, json=data)
+    reply = response.json()
+    print(reply)
 
 
 if __name__ == '__main__':
